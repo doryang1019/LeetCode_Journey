d23cea174b28bf1d8a4edb15b94e960c5a98f128
longnh238 <longnh238@gmail.com>
2025-01-12 12:36:30 -0800

    Added Code for Invert Binary Tree

---
+++ 4. Trees/1. Invert Binary Tree/long.py
@@ -0,0 +1,27 @@
+# Runtime 0ms
+# Beats 100.00%
+
+# Memory 17.95MB
+# Beats 13.54%
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return root
+
+        # Swap the left and right subtrees
+        root.left, root.right = root.right, root.left
+
+        # Recursively invert the left and right subtrees
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+        
