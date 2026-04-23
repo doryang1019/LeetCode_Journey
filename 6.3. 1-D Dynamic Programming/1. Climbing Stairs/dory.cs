dc47eaeaeded704a702e444dd41e27ca80e92f68
Long Nguyen <86302349+longnh238@users.noreply.github.com>
2026-04-23 09:50:52 -0700

    Update dory.cs

--- 6.3. 1-D Dynamic Programming/1. Climbing Stairs/dory.cs
+++ 6.3. 1-D Dynamic Programming/1. Climbing Stairs/dory.cs
@@ -1,1 +1,23 @@
+// Runtime 0ms
+// Beats 100.00%
 
+// Memory 28.83MB
+// Beats 82.56%
+
+public class Solution {
+    public int ClimbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
+    }
+}
