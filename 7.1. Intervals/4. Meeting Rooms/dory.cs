c001ec9d84cbf06f277ce3083f4517b2b4b80eeb
Long Nguyen <86302349+longnh238@users.noreply.github.com>
2026-04-24 12:07:22 -0700

    Update dory.cs

--- 7.1. Intervals/4. Meeting Rooms/dory.cs
+++ 7.1. Intervals/4. Meeting Rooms/dory.cs
@@ -1,1 +1,28 @@
+# Runtime 132ms
+# Beats 61.36%
 
+# Memory 29.0 MB
+# Beats 84.09%
+
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool CanAttendMeetings(List<Driver.Interval> intervals) {
+        intervals.Sort((a, b) => a.start.CompareTo(b.start));
+        for(int i = 0; i < intervals.Count - 1; i++) {
+            if(intervals[i].end > intervals[i+1].start) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
